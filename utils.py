65bf2bfcb515895a9a6c2a7c0b0e2f967601b2ee
manikbhandari <mbbhandarimanik2@gmail.com>
2020-01-29 21:43:50 -0500

    Data processing almost complete. Need to save embedding file in a readable tensor format so that it can be looked up in pytorch.

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 import os
 import json
 import yaml
+import pickle
+import numpy as np
 from collections import Counter
 from data import get_xy
 
 
-def make_vocab(text, voc_fname, split, add_pad, num_words=None):
+def make_vocab(text, voc_fname, split, add_pad, add_unk, num_words=None):
     """
     Creates and stores vocabulary for text and labels. Needs to be run only once.
     Also adds padding token <PAD>
@@ -17,10 +19,10 @@ def make_vocab(text, voc_fname, split, add_pad, num_words=None):
     vocab_path = voc_fname
     if os.path.exists(vocab_path):
         print("vocab file exists at {}".format(vocab_path))
-        exit(0)
+        return
     # assume text is space separated. Might be better to use a proper word tokenizer
     if split:
-        words = [wrd for line in text for wrd in line.split(" ")]
+        words = [wrd for line in text for wrd in line.lower().split(" ")]
     else:
         words = text
     counter = Counter(words)
@@ -30,26 +32,71 @@ def make_vocab(text, voc_fname, split, add_pad, num_words=None):
     for wrd, cnt in word_counts:
         vocab[wrd] = idx
         idx += 1
+    if add_unk:
+        vocab[UNK_TOKEN] = idx
+        idx += 1
     if add_pad:
-        vocab['<PAD>'] = idx
+        vocab[PAD_TOKEN] = idx
     print("Creating vocab for {} words".format(len(vocab)))
     with open(voc_fname, 'w') as f:
         json.dump(vocab, f)
 
 
-def save_dataset_embedded(text, labels, text_embed_file, labels_voc_file):
-    wrd2vec = {}
-    for line in open(text_embed_file, 'r'):
-        words = line.split(" ")
-        wrd2vec[words[0]] = words[1:]
-    dim = len(wrd2vec['the'])
-    wrd2vec['<UNK>'] = [0] * dim
+def save_dataset(text, labels, output_path):
+    """
+    Saves dataset padded to max length of the data. Lower cases everything. This might be crucial.
+    :param output_path: where to save the pickled data
+    :param text: list of sentences
+    :param labels: list of labels
+    :return: None. Saves the pickled data to output path.
+    """
+    if os.path.exists(output_path):
+        print("output path {} exists".format(output_path))
+        return
+    label2id = json.load(open(config['vocab_labels_path']))
+    word2id = json.load(open(config['vocab_text_path']))
+    max_len = max([len(sent.split(" ")) for sent in text])
+    sentences_ids = []
+    lengths = []
+    for i, sent in enumerate(text):
+        sent = sent.lower()
+        words = sent.split(" ")
+        len_words = len(words)
+        lengths.append(len_words)  # save the true length of sentence and pad to max_len
+        if len_words < max_len:
+            words += [PAD_TOKEN] * (max_len - len_words)
+        sentence_ids = []
+        for word in words:
+            sentence_ids.append(word2id[word])
+        sentences_ids.append(sentence_ids)
+        if i % 10000 == 0:
+            print("processed {} sentences".format(i))
+    sentences = np.array(sentences_ids)
+    lengths_arr = np.array(lengths)
+    print("saving sentences of shape {}".format(sentences.shape))
+    print("saving lengths_arr of shape {}".format(lengths_arr.shape))
 
+    labels_ids = []
+    for i, label in enumerate(labels):
+        labels_ids.append(label2id[label])
+    labels_arr = np.array(labels_ids)
+    print("saving labels_arr of shape {}".format(labels_arr.shape))
+    data = {
+        'sentences': sentences,
+        'lengths': lengths_arr,
+        'labels': labels_arr
+    }
+    pickle.dump(data, open(output_path, 'wb'))
 
 
 if __name__ == "__main__":
     config = yaml.safe_load(open("config.yml", 'r'))
     DATAPATH = config['datapath']
+    PAD_TOKEN = config['pad_token']
+    UNK_TOKEN = config['unk_token']
     valid_text, valid_labels = get_xy(DATAPATH + "topicclass_valid.txt")
-    make_vocab(valid_text, DATAPATH + "test_vocab.json", split=True, add_pad=True)
-    make_vocab(valid_labels, DATAPATH + "test_vocab_labels.json", split=False, add_pad=False)
+    train_text, train_labels = get_xy(DATAPATH + "topicclass_train.txt")
+    make_vocab(train_text + valid_text, config['vocab_text_path'], split=True, add_pad=True, add_unk=True)
+    make_vocab(valid_labels + train_labels, config['vocab_labels_path'], split=False, add_pad=False, add_unk=False)
+    save_dataset(train_text, train_labels, config['train_data_path'])
+    save_dataset(valid_text, valid_labels, config['valid_data_path'])
